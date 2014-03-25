b5693ba3119454e3688ccc1c38deb8af6775dbfd
Andrea Fuller <andreanicolefuller@gmail.com>
2014-03-25 01:03:41 -0400

    testing

--- apts/naked.py
+++ apts/naked.py
@@ -71,13 +71,13 @@ for x in range (1,2):
                         google = 'https://www.google.com/maps/dir/Wall+Street+Journal,+1211+Avenue+of+the+Americas,+New+York,+NY+10036/%27%27/data=!3m1!4b1!4m12!4m11!1m5!1m1!1s0x89c258ff6b2dac81:0x8579374f9050c674!2m2!1d' + lon + '!2d' + lat + '!1m3!2m2!1d' + lon + '!2d' + lat + '!3e3'
                         r3 = requests.get(google)
                         soup3 = BeautifulSoup(r3.content)
+                        print 'x'
                         #getting the shortest route via subway + duration
-                        route = soup3.find('li', {'id': 'altroute_0'})
-                        duration = route.find('div', {'class': 'altroute-rcol altroute-info'})
-                        time = duration.string
-                        time = re.search('(.*?) min',time)
-                        time = time.group(1)
-                        print time
+                        # route = soup3.find('li', {'id': 'altroute_0'})
+                        # duration = route.find('div', {'class': 'altroute-rcol altroute-info'})
+                        # time = duration.string
+                        # time = re.search('(.*?) min',time)
+                        # time = time.group(1)
                         #if the commute time is < 35 minutes, continue
                         # if float(time) <= 35:
                 #             #getting our header info (apt location, type, price) from the listing
